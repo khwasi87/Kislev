d9dd754d2e46e0b63ec22b55ed85f97385af3a84
khwasi8 <khwasi.87@gmail.com>
2022-04-06 22:18:38 +0530

    added owl carousel

--- js/style.js
+++ js/style.js
@@ -15,3 +15,24 @@ $('.sub').click(function() {
     target.value = +target.value - 1;
   }
 });
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:true,
+          loop:false
+      }
+  }
+})
