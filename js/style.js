3c31bf2a83695db9f0111176dd0fd18f6933bbf0
khwasi8 <khwasi.87@gmail.com>
2022-04-06 22:49:52 +0530

    changes in owl carousel

--- js/style.js
+++ js/style.js
@@ -18,20 +18,19 @@ $('.sub').click(function() {
 
 $('.owl-carousel').owlCarousel({
   loop:true,
+  center: true,
+  autoHeight:true,
   margin:10,
   responsiveClass:true,
   responsive:{
       0:{
           items:1,
-          nav:true
       },
       600:{
-          items:3,
-          nav:false
+          items:2,
       },
       1000:{
-          items:5,
-          nav:true,
+          items:2,
           loop:false
       }
   }
